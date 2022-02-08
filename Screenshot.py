d39dcdfabf864abb27b9e38d74f08e7bee93e0c9
前田 <sloth19920731@gmail.com>
2022-02-08 23:51:41 +0900

    メール送信の準備

--- Screenshot.py
+++ Screenshot.py
@@ -1,27 +1,62 @@
 import pyautogui as pag
 from time import sleep
+import smtplib, ssl
+import requests
 
-import matplotlib.pyplot as plt
-import numpy as np
-import tenserflow as tf
-import tenserflow_datasets as tfds
-from tenserflow import keras
+# 画像認識の処理
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import tenserflow as tf
+# import tenserflow_datasets as tfds
+# from tenserflow import keras
 
-builder = tfds.builder('rock_paper_scissors')
-info = builder.info
-ds_train = tfds.load(name="rock_paper_scissors", split="train")
-ds_test = tfds.load(name="rock_paper_scissors", split="test")
+# builder = tfds.builder('rock_paper_scissors')
+# info = builder.info
+# ds_train = tfds.load(name="rock_paper_scissors", split="train")
+# ds_test = tfds.load(name="rock_paper_scissors", split="test")
+# fig = tfds.show_examples(info, ds_train)
 
-fig = tfds.show_examples(info, ds_train)
+
+
+# メール送信の処理
+
+def read_creds():
+    user = passw = ""
+    with open("credentials.txt", "r", encoding="utf-8") as f:
+        file = f.readlines()
+        user = file[0].strip()
+        passw = file[1].strip()
+
+    return user, passw
+
+port = 465
+
+sender, password = read_creds()
+
+receiver = sender
+
+message = """\
+Subject: Inventory Notification
+We have new inventory.
+"""
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender, password)
+        server.sendmail(sender, receiver, message)
+        print("メールを送信しました")
+
+
 
 # スクリーンショットの処理
-# i = 1
-# savepath = 'D:\portfolio\PythonScreenshot'
+i = 1
+savepath = 'D:\portfolio\PythonScreenshot'
 
-# try:
-#     while True:
-#         img = pag.screenshot(savepath + '/screenshot' + str(i) + '.png')
-#         sleep(10)
-#         i = i + 1
-# except KeyboardInterrupt:
-#     print('\n')
+try:
+    while True:
+        img = pag.screenshot(savepath + '/screenshot' + str(i) + '.png')
+        sleep(30)
+        i = i + 1
+except KeyboardInterrupt:
+    print('\n')
