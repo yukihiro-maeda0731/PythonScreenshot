7842d08bb53780808df6f04bb5abdbe708cf298e
前田 <sloth19920731@gmail.com>
2022-02-19 23:46:04 +0900

    !_src.empty() in function 'cv::cvtColor'エラー 10:00まで

--- Screenshot.py
+++ Screenshot.py
@@ -1,3 +1,7 @@
 import cv2
 
-img = cv2.imread('assets/logo.png', )
+img = cv2.imread('D:\portfolio\PythonScreenshot\assets\logo.jpg', 0)
+
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
